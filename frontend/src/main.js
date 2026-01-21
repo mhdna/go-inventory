a7ce13f4df778cd8ab9f9e9694539c391b793f2a
Mahdi <mhdna@outlook.com>
2026-01-21 18:29:03 +0200

    Use Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,18 @@ import App from './App.vue'
 import './style.css';
 import router from './router';
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: 'dark'
+    }
+})
+
+createApp(App).use(vuetify).use(router).mount('#app')
