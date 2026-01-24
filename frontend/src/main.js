b0a092fc0d1be9f691f4b24d5e718c0b9829d7ad
Mahdi <mhdna@outlook.com>
2026-01-24 14:23:39 +0200

    add mdi icon pack

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,11 @@ import './style.css';
 import router from './router';
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
     components,
