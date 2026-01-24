f549ead4b08141da68d025c6d284837c392593ae
Mahdi <mhdna@outlook.com>
2026-01-24 05:06:39 +0200

    some semicolons won't hurt

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,28 +1,28 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router';
 
-import Login from '../views/Login.vue'
-import About from '../views/About.vue'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue';
+import About from '../views/About.vue';
+import Home from '../views/Home.vue';
 
 const routes = [
     { path: '/', component: Login },
     { path: '/home', component: Home },
     { path: '/about', component: About },
-]
+];
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes,
-})
+});
 
 router.beforeEach((to, from, next) => {
     const isAuthenticated = localStorage.getItem('authToken');
 
     if (to.meta.requiresAuth && !isAuthenticated) {
-        next("/")
+        next("/");
     } else {
         next();
     }
-})
+});
 
 export default router;
