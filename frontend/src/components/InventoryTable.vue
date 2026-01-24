f6e6ccb002365681a18ccdde6260e9042ecf28e7
Mahdi <mhdna@outlook.com>
2026-01-24 05:26:04 +0200

    add inventory table

---
+++ frontend/src/components/InventoryTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-data-table
+      :items="items"
+      :loading="loading"
+      fixed-header
+      height="520">
+    </v-data-table>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  items: Array,
+  loading: Boolean
+})
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+import { GetItems } from '../../wailsjs/go/main/App'
+import InventoryTable from '../components/InventoryTable.vue'
+import CreateItem from '../components/CreateItem.vue'
+
+const loading = ref(true)
+const search = ref('')
+const items = ref([])
+
+async function loadItems() {
+  loading.value = true
+  items.value = await GetItems(search.value)
+  loading.value = false
+}
+
+onMounted(() => loadItems())
+</script>
+
 <template>
-    Welcome
+  <v-container>
+    <InventoryTable :items="items" :loading="loading" />
+  </v-container>
 </template>
+
